a27e69b0bbf6edd7101cd9215f9a77dcd07dedab
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2021-05-25 20:32:05 -0300

    modificação de soma

--- trabalho de python/trabalho 1.py
+++ trabalho de python/trabalho 1.py
@@ -1,2 +1,4 @@
-n1 = int(input('Informe um número:'))
-print('número informado{}'.format(n1))
+n1=int(input('Informe o primeiri número:'))
+n2=int(input('Informe outro número'))
+n3=n1+n2
+print('Á \033[1;33msoma\033[m entre os número {} e {} é igual a {}'.format(n1,n2,n3))
