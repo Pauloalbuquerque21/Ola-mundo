85082bacb91b3eeb663cf4f20a31cd24da945903
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2021-12-23 08:36:50 -0300

    terminando exercicio 36

---
+++ exercicio 36.py
@@ -0,0 +1,12 @@
+n1 = int(input('Qual o Valor da casas:'))
+n2 = int(input('Qual o seu salário:'))
+n3 = int(input('Quantos anos você pensa em pagar:'))
+n4=(n1/n3)/12
+n5 = (n2*30)/100
+if n4 <= n5:
+    print('podemos conseder esse emprestimo')
+    print(f'30% do salario{n5},Parcela{n4:.2f}')
+else:
+    print('Não podemos te conseder esse emprestimo.')
+
+#print(f'{n1},{n2},{n3},{n4:.2f}')
