b7ddab0d1e838cdc3bb97bf7903c107cd0073e3d
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-01-02 18:23:15 -0300

    terminei o exercicio 45

--- exercicio 45.py
+++ exercicio 45.py
@@ -1,7 +1,8 @@
 from time import sleep
 import random
 list = ('PEDRA','PAPEL','TESOURA')
-n2 = random.choice(list)
+list2 = (0,1,2)
+n2 = random.choice(list2)
 n1 = int(input('Suas opções:\n[0]PEDRA\n[1]PAPEL\n[2]TESOURA\nQual é a sua jogada?'))
 print('JO')
 sleep(1)
@@ -11,15 +12,30 @@ print('PO!!!')
 sleep(1)
 print('-=-'*20)
 print(f'O jogador escolheu {list[n1]}')
-print(f'A maquina escolheu {n2}')
+print(f'A maquina escolheu {list[n2]}')
 print('-=-'*20)
 if n2 == 0:
     if n1 == 0:
         print('Empate')
     elif n1 == 1:
         print('O jogador ganhou')
-    
-
+    elif n1 == 2:
+        print('A maquina ganhou')
+if n2 == 1:
+    if n1 == 0:
+        print('A maquina ganhou')
+    elif n1 == 1:
+        print('Empate')
+    elif n1 == 2:
+        print('O jogador ganhou')
+if n2 == 2:
+    if n1 == 0:
+        print('O jogador ganhou')
+    elif n1 == 1:
+        print('A maquina ganhou')
+    elif n1 == 2:
+        print('Empate')
+print('-=-'*20)
 
 
 
