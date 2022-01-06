a5826a0f57d630b550e809e47a68931b27279a28
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-01-06 14:05:29 -0300

    atividade em processo

---
+++ exercicio 47.py
@@ -0,0 +1,5 @@
+from time import sleep
+n1 = int(input('Digite o primeiro número, tem que ser número par:'))
+n2 = int(input('Digite o último, tem que ser número:'))
+for c in range(n1,n2,2):
+    print(c, end=' ')
