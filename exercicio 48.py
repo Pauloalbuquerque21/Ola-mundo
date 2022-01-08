a7310731b03654ccf91f8dbe7428258daee448b9
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-01-08 17:53:18 -0300

    terminado

--- exercicio 48.py
+++ exercicio 48.py
@@ -1,5 +1,7 @@
-print('Segue a soma de todos os números entre 1 e 500')
 soma=0
+quant=0
 for c in range(1,500,2):
-    soma=soma+c
-print(soma)
+    if c%3 == 0:
+        soma=soma+c
+        quant=quant+1
+print(f'Soma:{soma}\nQuantidade: {quant}')
