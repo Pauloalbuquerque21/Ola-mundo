7fd708699bbc0defc979c8298328c7087cdee984
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-01-13 16:35:43 -0300

    terminei

--- exercicio 55.py
+++ exercicio 55.py
@@ -1,16 +1,19 @@
+from termios import NL1
+
+
 maior = 0
 menor = 0
 for c in range(1,6):
     n1 = float(input(f'Peso da {c} pessoa:'))
     if c == 1:   
-        maior = c
-        menor = c
+        maior = n1
+        menor = n1
     else:
         if n1 > maior:
             maior = n1
         if n1 < menor:
             menor = n1
-print(f'O Maior peso é {maior}')
-print(f'O menor peso é {menor}')
+print(f'O Maior peso é {maior}Kg')
+print(f'O menor peso é {menor}Kg')
 
 
