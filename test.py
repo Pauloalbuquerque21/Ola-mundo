72044336d13512e238fe4d22829a9d9625324b73
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-28 21:39:06 -0300

    em processo

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ n3 = int(input('Teste 3:'))
 n4 = int(input('Teste 4:'))
 lista=[n1,n2,n3,n4]
 random.shuffle(lista)
-n3 = n1 + n2 + n3 + n4
+n5 = n1 + n2 + n3 + n4
 print('O número da escola é \n{}'.format(lista))
 print(lista)
