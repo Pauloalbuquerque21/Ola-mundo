02487122c85fbe16ff7a05b6138851a2d82bec82
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-30 15:53:58 -0300

    revendo aprendizados

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-import random
-n1 = int(input('Teste 1:'))
-n2 = int(input('Teste 2:'))
-n3 = int(input('Teste 3:'))
-n4 = int(input('Teste 4:'))
-lista=[n1,n2,n3,n4]
-random.shuffle(lista)
-n5 = n1 + n2 + n3 + n4
-print('O número da escola é \n{}'.format(lista))
-print(lista)
+#import random
+#n1 = int(input('Teste 1:'))
+#n2 = int(input('Teste 2:'))
+#n3 = int(input('Teste 3:'))
+#n4 = int(input('Teste 4:'))
+#lista=[n1,n2,n3,n4]
+#random.shuffle(lista)
+#n5 = n1 + n2 + n3 + n4
+#print('O número da escola é \n{}'.format(lista))
+#print(lista)
+nome=str(input('Qual o seu nome?'))
+responta=str(input('Ola {}, pode me responder algumas perguntas?')).
+n1 = float(input('Digite um valor:'))
+n2 = float(input('Digite outro valor '))
