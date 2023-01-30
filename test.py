4a24b7238dd09c1afae5fff94d41cd5e4409b0e4
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-30 17:03:40 -0300

    praticando

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from time import sleep
 #import random
 #n1 = int(input('Teste 1:'))
 #n2 = int(input('Teste 2:'))
@@ -8,7 +9,11 @@
 #n5 = n1 + n2 + n3 + n4
 #print('O número da escola é \n{}'.format(lista))
 #print(lista)
-nome=str(input('Qual o seu nome?'))
-responta=str(input('Ola {}, pode me responder algumas perguntas?')).
-n1 = float(input('Digite um valor:'))
-n2 = float(input('Digite outro valor '))
+nome=str(input('Qual o seu nome?')).strip()
+resposta=str(input('Ola {}, pode me responder algumas perguntas?'.format(nome))).strip().lower()
+if resposta == "sim" or  resposta == "s":
+    n1 = float(input('Digite um valor:'))
+    n2 = float(input('Digite outro valor '))
+else:
+    print('ok, obrigado')
+    sleep(1)
