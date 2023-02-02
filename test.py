c6bb64c2b2316e5b6b416c22ec3a11c1b498f7fc
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-02-02 08:13:37 -0300

    em processo de aprendizado

--- test.py
+++ test.py
@@ -9,11 +9,18 @@ from time import sleep
 #n5 = n1 + n2 + n3 + n4
 #print('O número da escola é \n{}'.format(lista))
 #print(lista)
+print('{:=^40}'.format('Tabulada'))
 nome=str(input('Qual o seu nome?')).strip()
 resposta=str(input('Ola {}, pode me responder algumas perguntas?'.format(nome))).strip().lower()
 if resposta == "sim" or  resposta == "s":
     n1 = float(input('Digite um valor:'))
     n2 = float(input('Digite outro valor '))
+    soma = n1 + n2
+    mult = n1 * n2
+    div = n1 / n2
+    rest = n1 // n2
+    pont = n1 ** n2
+    print('Soma:{}\nMultiplicação:{}\nDivisão:{}\nResto da divisão:{}\nPontenciação{}'.format(soma,mult,div,rest,pont))
 else:
     print('ok, obrigado')
     sleep(1)
