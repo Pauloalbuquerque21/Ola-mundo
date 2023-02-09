b1ab92b5948e547f12c22b4a4c9e702b83c368d7
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-02-09 06:26:04 -0300

    revendo python

--- exercicio 47.py
+++ exercicio 47.py
@@ -2,4 +2,4 @@ from time import sleep
 n1 = int(input('Digite o primeiro número, tem que ser número par:'))
 n2 = int(input('Digite o último, tem que ser número:'))
 for c in range(n1,n2,2):
-    print(c, end=' ')
+    print(c, end='')
