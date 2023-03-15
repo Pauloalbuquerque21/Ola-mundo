314342c21df1ae192d2e0b535d3cd868fe05be16
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-03-15 07:40:51 -0300

    aprendendo usar repetição na tupla

--- python mundo 3/aula16tuplas.py
+++ python mundo 3/aula16tuplas.py
@@ -1,2 +1,7 @@
-lanche = ('Hambúrguer','Suco','Pizza','Pudim')
-print(lanche[0:])
+lanche = ('Hambúrguer','Suco','Pizza','Pudim', 'Batata Frita')
+
+for cont in range(0,len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}')
+
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
