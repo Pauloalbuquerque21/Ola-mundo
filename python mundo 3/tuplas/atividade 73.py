f7ef7b2c881c928bd653759492129a4dcd4dfbbe
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-07 21:05:26 -0300

    em processo

--- python mundo 3/tuplas/atividade 73.py
+++ python mundo 3/tuplas/atividade 73.py
@@ -1,9 +1,12 @@
 colocação = ('América-MG','Athletico-PR','Atlético-MG','Bahia','Botafogo','Corinthians','Coritiba','Cruzeiro','Cuiabá','Flamengo','Fluminense','Fortaleza','Goiás','Grêmio','Internacional','Palmeiras','Bragantino','Santos','São Paulo','Vasco')
-primeiros=int(input('Quais os primeiros colocados que você quer ver de 1 a 20'))
-ultimos=int(input('Quais os ultimos colocados'))
+primeiros=int(input('Quais os primeiros colocados que você quer ver de 1 a 20:'))
+ultimos=int(input('Quais os ultimos colocados:'))
+posição=str(input('Digite seu time:')).strip().capitalize()
 print('Os primeiros:')
 for c in range(0,primeiros):
    print(colocação[c])
 print('Os Últimos:')
 for c in range(19,20-(ultimos+1),-1):
     print(colocação[c])
+print(sorted(colocação))
+print(f'o {posição} está na posição {colocação.index((posição+1))}')
