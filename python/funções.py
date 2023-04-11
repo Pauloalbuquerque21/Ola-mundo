6ef72d9b76a0a365b25e7da1a31873136cdbe42d
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-11 20:18:15 -0300

    em processo

--- python/funções.py
+++ python/funções.py
@@ -1,3 +1,32 @@
 #Funções
 
-def<multiplicar>()
+#funções sem argumento
+#def mensagem():
+#    print('mensagem')
+
+#mensagem()
+
+#Função com argumento simples
+#x = 10
+#y = 20
+
+#def soma (a,b):
+#    return a + b
+
+#r = soma(x , y)
+#print(r)
+#print(f'A soma entra {x} e {y} é igual {r}')
+
+#Função mais complexa
+
+valores = [1,2,3,4,5]
+def quadrado(valores):
+    quadrados = []
+    for x in valores:
+        quadrados.append(x**2)
+    return quadrados
+    
+resultados = quadrado(valores)
+
+for y in resultados:
+    print(y)
