5d21e27708117e1b3a7be3c73d3249e963818cd8
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-11 22:20:37 -0300

    parei

--- python/python mundo 3/tuplas/atividade 73.py
+++ python/python mundo 3/tuplas/atividade 73.py
@@ -1,12 +1,14 @@
 colocação = ('América-MG','Athletico-PR','Atlético-MG','Bahia','Botafogo','Corinthians','Coritiba','Cruzeiro','Cuiabá','Flamengo','Fluminense','Fortaleza','Goiás','Grêmio','Internacional','Palmeiras','Bragantino','Santos','São Paulo','Vasco')
-primeiros=int(input('Quais os primeiros colocados que você quer ver de 1 a 20:'))
+primeiros=int(input('Quantos dos primeiros colocados que você quer ver de 1 a 20:'))
 ultimos=int(input('Quais os ultimos colocados:'))
 posição=str(input('Digite seu time:')).strip().capitalize()
 print('Os primeiros:')
 for c in range(0,primeiros):
    print(colocação[c])
+print('-='*15)
 print('Os Últimos:')
 for c in range(19,20-(ultimos+1),-1):
     print(colocação[c])
+print('-='*15)
 print(sorted(colocação))
-print(f'o {posição} está na posição {colocação.index((posição+1))}')
+print(f'o {posição} está na posição {(colocação.index((posição)))+1}')
--- python/python mundo 3/tuplas/aula16tuplas.py
+++ python/python mundo 3/tuplas/aula16tuplas.py
@@ -1,8 +1,11 @@
 entrada = 0
 while True:
     entrada = int(input('Digite um valor entre 0 e 20:'))
-    if entrada == 11:
-        break
+    if 20 >= entrada >= 0:
+        if entrada == 11:
+            break
+        else:
+            a = ('Zero','Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete','Dezoito','Dezenove','Vinte')
+            print(f'O valor digitado é {a[entrada]}')
     else:
-        a = ('Zero','Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete','Dezoito','Dezenove','Vinte')
-        print(f'O valor digitado é {a[entrada]}')
+        print('Numero invalido, tente novamente.')
