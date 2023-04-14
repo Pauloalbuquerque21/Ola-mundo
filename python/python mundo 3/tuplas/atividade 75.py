704663e11429fb5114c77114280872d135e64dbf
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-13 22:08:03 -0300

    terminando o exercicio 75

---
+++ python/python mundo 3/tuplas/atividade 75.py
@@ -0,0 +1,7 @@
+op1 = int(input('Digite um número:'))
+op2 = int(input('Digite outro número:'))
+op3 = int(input('Digite mais um número:'))
+op4 = int(input('Digite o ultimo número:'))
+
+ops = (op1,op2,op3,op4)
+print(f'Você digitou os valores {ops}')
