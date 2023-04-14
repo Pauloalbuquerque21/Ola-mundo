6ce2d82b75fb6b55560a62d0bbf5d4cafdcc752a
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-13 22:28:44 -0300

    parei

--- python/python mundo 3/tuplas/atividade 75.py
+++ python/python mundo 3/tuplas/atividade 75.py
@@ -1,7 +1,18 @@
-op1 = int(input('Digite um número:'))
-op2 = int(input('Digite outro número:'))
-op3 = int(input('Digite mais um número:'))
-op4 = int(input('Digite o ultimo número:'))
-
-ops = (op1,op2,op3,op4)
-print(f'Você digitou os valores {ops}')
+num = (int(input('Digite um número:')), 
+       int(input('Digite outro número:')), 
+       int(input('Digite mais um número:')), 
+       int(input('Digite o último número:')))
+                                           
+print(f'Você digitou os valores {num}')
+if 9 in num:
+    print(f'Você digitou {num.count(9)} vezes, o valor 9')
+else:
+    print(f'Você não digitou o valor 9')
+if 3 in num:
+    print(f'você digitou valor 3 na {(num.index(3))+1} posição')
+else:
+    print(f'você não digitou o valor 3')
+print(f'Os valores pares são:',end=' ')
+for n in num:
+    if n % 2 == 0:
+        print(f'{n}', end=' ')
