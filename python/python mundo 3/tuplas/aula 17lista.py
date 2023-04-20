d1948affea45f83154bd1c471e1de1b69c7f6127
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-20 13:48:54 -0300

    parte prática

---
+++ python/python mundo 3/tuplas/aula 17lista.py
@@ -0,0 +1,17 @@
+#lanche = [1,2,3,4]
+#lanche[0] = 4
+#print(lanche)
+#lanche.pop(3)
+#lanche.remove(4)
+#del lanche[0]
+#print(lanche)
+#if 1 in lanche:
+#    lanche.remove(1)
+#else:
+#    lanche.append(1)
+#print(lanche)
+num = [2,5,9,1]
+num[2] = 3
+num.append(7)
+num.sort()
+print
