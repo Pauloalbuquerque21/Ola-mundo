8100eda4324b76cd4eeae7f6750bff51309b26a6
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-24 10:30:58 -0300

    em processo

--- python/python mundo 3/tuplas/exercicio 82.py
+++ python/python mundo 3/tuplas/exercicio 82.py
@@ -2,17 +2,21 @@ lista = []
 impar = []
 par = []
 while True:
-    valores = int(input('Digite um número:'))
+    lista.append(int(input('Digite um número:')))
     opc = input('Quer continuar? [S/N]')
-    lista.append(valores)
+    
+#    if valores % 2 == 0:
+#        par.append(valores)
+#    if valores % 2 != 0:
+#        impar.append(valores)
     if opc in 'Nn':
         break
+for i , v in enumerate(lista):
+    if v % 2 == 0:
+        par.append(v)
     else:
-        if valores % 2 == 0:
-            par.append(valores)
-        else:
-            impar.append(valores)
-
+        impar.append(v)
+     
 print(f'Os números digitados foram {lista}')
 print(f'Os números impares digitados foram {impar}')
 print(f'Os números pares digitados foram {par}')
