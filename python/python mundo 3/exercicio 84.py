f7355beadc3d8dfac06d974759aa67288b9699c8
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-27 20:21:25 -0300

    em procsso

--- python/python mundo 3/exercicio 84.py
+++ python/python mundo 3/exercicio 84.py
@@ -1,22 +1,24 @@
-resultado = list()
-dados=list()
+resultado =[]
+dados=[]
 total = 0
 maior = 0 
 menor = 0 
 while True:
     dados.append(str(input('Nome:')))
     dados.append(int(input('Peso:')))
+    if len(dados) == 0:
+        maior = menor = dados[1]
+    else:
+        if dados[1] > maior:
+            maior = dados[1]
+        if dados[1] < menor:
+            menor = dados[1]
     resultado.append(dados[:])
     dados.clear()
     total+=1
     contin = str(input('Deseja continuar [S/N]:'))
     if contin in 'Nn':
         break
-for c in resultado:
-    if c[1] > maior:
-        c = maior
-    if c[1] < menor:
-        c = menor
 print('=-'*30)
 print(f'Ao todo, você cadastrou {total} pessoas')
 print(f'O maior peso foi de {maior}. Peso de ')
