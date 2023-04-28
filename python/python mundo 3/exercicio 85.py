da8da56f8b6565532a45ad0f734e53c095388907
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-28 07:25:09 -0300

    em processo

--- python/python mundo 3/exercicio 85.py
+++ python/python mundo 3/exercicio 85.py
@@ -5,9 +5,15 @@ for c in range(0,7):
     valores=int(input('Digite um valor:'))
     if valores % 2 == 0 :
         pares.append(valores)
-    else:
+    elif valores % 2 != 0:
         impares.append(valores)
-    
-print(f'Impares:{sorted(impares)}')
-print(f'Pares {sorted(pares)}')
+dados.append(pares[:])
+pares.clear
+dados.append(impares[:])
+impares.clear
+
+for c in dados[0]:
+    print(f'Pares {c}')
+for c in dados[1]:
+    print(f'Impares {c}')
 
