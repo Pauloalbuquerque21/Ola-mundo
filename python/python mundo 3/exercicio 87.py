79f197e7d04df57564ce2a4cf714a54fe6de0769
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-30 20:38:12 -0300

    finish

--- python/python mundo 3/exercicio 87.py
+++ python/python mundo 3/exercicio 87.py
@@ -13,12 +13,14 @@ for l in range(0,3):
 for l in range(0,3):
     for c in range(0,3):
         if matriz[l][c] % 2 == 0:
-            resul= resul + matriz[l][c]
+            resul+= matriz[l][c]
 for c in range(0,3):
-    if matriz[1][c] > maior:
-        matriz = maior
-    if matriz[1][c] < menor:
-        matriz[1][c] = menor
+    if c == 0:
+        maior = menor = matriz[1][c]
+    elif matriz[1][c] > maior:
+        maior = matriz[1][c]
+    elif matriz[1][c] < menor:
+        menor = matriz[1][c]
 print(f'A soma de todo os valores pares  é igual a: {resul}.')
-print(f'A soma dos valores da terceira coluna: {matriz[2][0]+matriz[2][1]+matriz[2][2]}.')
+print(f'A soma dos valores da terceira coluna: {matriz[0][2]+matriz[1][2]+matriz[2][2]}.')
 print(f'O menor valor da segunda linha é {menor} e o maior {maior}')
