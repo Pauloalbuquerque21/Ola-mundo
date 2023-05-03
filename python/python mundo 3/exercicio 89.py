5a597ea8e063df41bb321ed289db9a3063fe542e
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-03 06:54:14 -0300

    termino outro dia

--- python/python mundo 3/exercicio 89.py
+++ python/python mundo 3/exercicio 89.py
@@ -7,6 +7,12 @@ while True:
     ind.append(float(input('Nota 1:')))
     ind.append(float(input('Nota 2:')))
     ater= str(input('Quer continuar? [S/N]'))
-
+    inf.append(ind[:])
+    ind.clear()
     if ater in 'nN':
         break
+print('-'*30)
+for c in range(0,len(inf)):
+    print(f'{c} {inf[c][0]}  {(inf[c][1]+inf[c][2])/2}')
+print('-'*30)
+
