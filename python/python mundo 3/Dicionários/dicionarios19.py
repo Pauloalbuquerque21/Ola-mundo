dc214cafd7438f2b90549371a682308e3730d4bc
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-05 07:37:26 -0300

    em processo

--- python/python mundo 3/Dicionários/dicionarios19.py
+++ python/python mundo 3/Dicionários/dicionarios19.py
@@ -1,5 +1,11 @@
 #pessoas = {'nome':'Gustavo','sexo':'M','Idade':22}
-pessoas['nome'] = 'Leandro'
-pessoas['peso'] = 98.5
-for k, v in pessoas.items():
-    print(f'{k} = {v}')
+#pessoas['nome'] = 'Leandro'
+#pessoas['peso'] = 98.5
+#for k, v in pessoas.items():
+#    print(f'{k} = {v}')
+brasil = []
+estado1 = {'uf': 'Rio de janeiro','Sigla':'RJ'}
+estado2 = {'uf': 'São Paulo', 'Sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil[1]['sigla'])
