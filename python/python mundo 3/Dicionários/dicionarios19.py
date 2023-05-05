417d4db1cb420687034b0426cc202b3ea4c81c36
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-05 08:14:36 -0300

    Update dicionarios19.py

--- python/python mundo 3/Dicionários/dicionarios19.py
+++ python/python mundo 3/Dicionários/dicionarios19.py
@@ -3,9 +3,21 @@
 #pessoas['peso'] = 98.5
 #for k, v in pessoas.items():
 #    print(f'{k} = {v}')
-brasil = []
-estado1 = {'uf': 'Rio de janeiro','Sigla':'RJ'}
-estado2 = {'uf': 'São Paulo', 'Sigla': 'SP'}
-brasil.append(estado1)
-brasil.append(estado2)
-print(brasil[1]['sigla'])
+#----------------------------------------------------------#
+#brasil = []
+#estado1 = {'uf': 'Rio de janeiro','Sigla':'RJ'}
+#estado2 = {'uf': 'São Paulo', 'Sigla': 'SP'}
+#brasil.append(estado1)
+#brasil.append(estado2)
+#print(brasil[1]['sigla'])
+
+estado = dict()
+brasil = list()
+for c in range(0,3):
+    estado['uf'] = str(input('Unidade Federativa:'))
+    estado['sigla'] = str(input('Sigla do Estado:'))
+    brasil.append(estado.copy())
+for e in brasil:
+    for v in e.values():
+        print(v, end=' ')
+    print()
