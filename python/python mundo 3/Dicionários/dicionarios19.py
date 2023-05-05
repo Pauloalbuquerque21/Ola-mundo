72711ee2fd06587a0d9c55a28412583a4438cc35
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-05 07:32:04 -0300

    Update dicionarios19.py

--- python/python mundo 3/Dicionários/dicionarios19.py
+++ python/python mundo 3/Dicionários/dicionarios19.py
@@ -1,4 +1,4 @@
-pessoas = {'nome':'Gustavo','sexo':'M','Idade':22}
+#pessoas = {'nome':'Gustavo','sexo':'M','Idade':22}
 pessoas['nome'] = 'Leandro'
 pessoas['peso'] = 98.5
 for k, v in pessoas.items():
