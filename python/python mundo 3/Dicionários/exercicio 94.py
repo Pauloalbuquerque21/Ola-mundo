5e4b28701ccb25b045ae25d566f05e62d657d2d3
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-10 18:20:22 -0300

    em processo

--- python/python mundo 3/Dicionários/exercicio 94.py
+++ python/python mundo 3/Dicionários/exercicio 94.py
@@ -1,14 +1,15 @@
 inf = dict()
-nome = alt = idade = sexo = 0
+inf2 = list()
+nome = alt = idade = sexo = result = 0
 while True:
     nome = str(input('NOME:'))
-    sexo = str(input('SEXO:[M/F]'))
+    sexo = str(input('SEXO:[M/F]')).upper()
     while True:
         if sexo in 'mMfF':
             break
         elif sexo != 'mM' or sexo != 'fF':
             print('errado')
-            sexo=str(input('SEXO:[M/F]'))
+            sexo=str(input('SEXO:[M/F]')).upper()
     idade = int(input('Idade:'))
     while True:
         alt = str(input('Quer continuar? [S/N]'))
@@ -16,13 +17,19 @@ while True:
             break
         elif alt != 'nNsS':
             print('Errado')
-    inf["nome"].append(nome)
-    inf["sexo"].append(sexo)
-    inf['idade'].append(idade)
-    
+    inf["nome"]=nome
+    inf["sexo"]=sexo
+    inf['idade']=idade
+    inf2.append(inf.copy())
+    inf.clear()
     if alt in 'nN':
         break
-print(f"{inf}")
+print('-='*30)
+for y in range(0,len(inf2)):
+    for c in inf2[y][]:
+        result = c + result
+print(f'A) Ao todo temos {len(inf2)} pessoas cadastradas.')
+print(f'B) A média de idade é de {result} anos.')
 
 
 
