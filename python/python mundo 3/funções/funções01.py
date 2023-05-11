137bb71fecef6d4577978804d0620ebfc35cb23b
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-11 19:23:09 -0300

    em processo

---
+++ python/python mundo 3/funções/funções01.py
@@ -0,0 +1,14 @@
+def mostralinha():
+    print('-------------------')
+
+mostralinha()
+print('Sistema de alunos')
+mostralinha()
+mostralinha()
+print(' cadastro de funções')
+
+def mensagem(msg):
+    print('--------------')
+    print(msg)
+    print('--------------')
+mensagem('escola')
