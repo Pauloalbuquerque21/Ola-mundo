37044aa2546fc5fe5e417bd5fcc8e9f0bd553ef5
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-12 19:15:31 -0300

    iniciando atividade

---
+++ python/python mundo 3/funções/exercicio 96.py
@@ -0,0 +1,9 @@
+print('Controle de  Terrenos')
+print('-'*30)
+n1 = float(input('LARGURA (m):'))
+n2 = float(input('COMPRIMENTO (m):'))
+
+def inf(a,b):
+    print(f'A área de um terreno {a}x{b} é de {a*b}')
+
+inf(n1,n2)
