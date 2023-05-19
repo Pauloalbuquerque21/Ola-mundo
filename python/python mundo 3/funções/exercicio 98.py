0cd9f5a39a5259d56e40bc4ee8d63f31ffab7261
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-19 19:41:26 -0300

    em processo

--- python/python mundo 3/funções/exercicio 98.py
+++ python/python mundo 3/funções/exercicio 98.py
@@ -1,7 +1,18 @@
 from time import sleep
+inf = list()
 print('-='*20)
 print('Contagem de 1 até 10 de 1 em 1')
 for c in range(0,11):
     print(f'{c}',end=' ', flush = True)
     sleep(0.5)
 print('FIM')
+print('-='*20)
+print('Agora é sua vez de personalizar a contagem')
+inf.append(int(input('Início:')))
+inf.append(int(input('Fim:')))
+inf.append(int(input('Passo:')))
+print('-='*20)
+print(f'{inf},{inf[0]},{inf[1]},{inf[2]}')
+for c in range(inf[0],inf[2],inf[1]):
+    print(f'{c}',end=' ',flush = True)
+    sleep(0.5)
