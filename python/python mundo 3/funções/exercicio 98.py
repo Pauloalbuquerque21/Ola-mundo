bc7bd6050ad38de80164b535ff4eebbb3ec100f0
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-19 19:02:57 -0300

    Update exercicio 98.py

--- python/python mundo 3/funções/exercicio 98.py
+++ python/python mundo 3/funções/exercicio 98.py
@@ -2,6 +2,6 @@ from time import sleep
 print('-='*20)
 print('Contagem de 1 até 10 de 1 em 1')
 for c in range(0,11):
-    print(f'{c}', end=' ')
-    sleep(1)
+    print(f'{c}',end=' ', flush = True)
+    sleep(0.5)
 print('FIM')
