1bddfca141166a0c4137cf2d3ddb266a2ada6ab0
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-19 19:58:31 -0300

    terminei parte da atividade

--- python/python mundo 3/funções/exercicio 98.py
+++ python/python mundo 3/funções/exercicio 98.py
@@ -11,8 +11,12 @@ print('Agora é sua vez de personalizar a contagem')
 inf.append(int(input('Início:')))
 inf.append(int(input('Fim:')))
 inf.append(int(input('Passo:')))
+if inf[0]>inf[1] and inf[2]>0:
+    n1 = inf[2] * -1
+    inf[2] = n1
+elif  
 print('-='*20)
 print(f'{inf},{inf[0]},{inf[1]},{inf[2]}')
-for c in range(inf[0],inf[2],inf[1]):
+for c in range(inf[0],inf[1],inf[2]):
     print(f'{c}',end=' ',flush = True)
     sleep(0.5)
