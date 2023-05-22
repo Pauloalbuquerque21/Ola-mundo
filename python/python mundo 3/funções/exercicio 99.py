39c95b7e27d4378e3ef599002aee4708d247ae3d
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-22 16:38:15 -0300

    aprendendo o código

--- python/python mundo 3/funções/exercicio 99.py
+++ python/python mundo 3/funções/exercicio 99.py
@@ -1,1 +1,21 @@
- 
+from time import sleep
+
+def maior(*núm):
+    cont = maior = 0
+    print('-='*20)
+    print('\nAnalisando os valores passados...')
+    for valor in núm:
+        print(f'{valor}',end=' ',flush=True)
+        sleep(0.3)
+        if cont == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        cont += 1
+    print(f'Foram informados {cont} valores ao todo.')
+    print(f'o maior valor informado foi {maior}.')
+
+# Programa principal
+maior(2, 9, 4, 5, 7, 1)
+maior (0,2)
