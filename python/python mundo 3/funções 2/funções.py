b7d20556a02c0b72bd763ac0323499c91f9c6e66
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-29 18:17:14 -0300

    terminei funções 2, partindo para prática

--- python/python mundo 3/funções 2/funções.py
+++ python/python mundo 3/funções 2/funções.py
@@ -17,14 +17,25 @@
 
 #soma(4,5,5)
 
-def teste():
-    x = 8
-    print(f'Na função teste, n vale {n}')
-    print(f'Na função teste, x vale {x}')
+
+
+
+#def teste():
+#    x = 8
+#    print(f'Na função teste, n vale {n}')
+#    print(f'Na função teste, x vale {x}')
 
 #programa pricipal
-n = 2 
-print(f'No programa principal, n vale {n}')
-teste()
-print(f'No programa principal, x vale {x}')
+#n = 2 
+#print(f'No programa principal, n vale {n}')
+#teste()
+#print(f'No programa principal, x vale {x}')
 
+def somar(a=0,b=0,c=0):
+    s = a + b + c 
+    return s
+r1 = somar(3,4,5)
+r2 = somar(3,8,9)
+r3 = somar(4,7,8)
+
+print(f'Os resultados foram {r1}, {r2} e {r3} ')
