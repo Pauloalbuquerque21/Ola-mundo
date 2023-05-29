6d0a4fc324bd7324b3a00edc5a0d097e5a4a356a
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-29 17:13:07 -0300

    Update funções.py

--- python/python mundo 3/funções 2/funções.py
+++ python/python mundo 3/funções 2/funções.py
@@ -1,5 +1,30 @@
-help(print)
-def contador(i,f,p):
-    c = i
-    while c <= f:
-        print(f'')
+#def contador(i,f,p):
+#    c=1
+#    while c <= f:
+#        print(f'{c}',end='..')
+#        c += p
+#    print('FIM!')
+
+#def soma(a=0,b=0,c=0):
+#    """
+#    ->
+#    a: uma variavel
+#    b: 
+
+#    """
+#    s = a + b + c
+#    print(f'A soma vale {s}')
+
+#soma(4,5,5)
+
+def teste():
+    x = 8
+    print(f'Na função teste, n vale {n}')
+    print(f'Na função teste, x vale {x}')
+
+#programa pricipal
+n = 2 
+print(f'No programa principal, n vale {n}')
+teste()
+print(f'No programa principal, x vale {x}')
+
