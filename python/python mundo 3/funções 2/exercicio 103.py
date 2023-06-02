ca5f9c97f0cb9b605a5b8e511b5ee433570481fe
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-06-02 09:10:00 -0300

    terminei atividade 103

--- python/python mundo 3/funções 2/exercicio 103.py
+++ python/python mundo 3/funções 2/exercicio 103.py
@@ -1,27 +1,43 @@
-dados = list()
-jogador = str(input('Nome do jogador:'))
-gols = str(input('Número de GOls:'))
+#dados = list()
+#jogador = str(input('Nome do jogador:')).strip()
+#if len(jogador) == 0:
+#    jogador = 0
+#gols = str(input('Número de GOls:')).strip()
+#if len(gols) == 0:
+#    gols = 0
+#def jog(a=0):
+#    infjog = 0
+#    if a == 0:
+#        infjog ='<Desconhecido>'
+#    else:
+#        infjog = a
+#    return infjog
+#def gol(b=0):
+#    infgol = 0 
+#    if b == 0:
+#        infgol = 0
+#    else:
+#        infgol = b
+#    return infgol
+#dados.append(jog(jogador))
+#dados.append(gol(gols))
 
 
-def jog(a=0):
-    infjog = 0
-    if a == 0:
-        infjog ='<Desconhecido>'
-    else:
-        infjog = a
-    return infjog
-def gol(b=0):
-    infgol = 0 
-    if b == 0:
-        infgol = 0
-    else:
-        infgol = b
-    return infgol
+#print(f'O jogador {dados[0]} fez gols(s) no compeonato')
 
+def ficha(job="Desconhecido", gol = 0):
+    print(f'O gador {jog} fez {gol} gol(s) no compeonato')
 
-dados.append(jog(jogador))
-dados.append(gol(gols))
-print(f'{dados}')
+#Programa principal
+n = str(input('Nome do jogador:'))
+g = str(input('Número de Gols'))
 
-
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0 
+if n.strip() == "":
+    ficha(gol=g)
+else:
+    ficha(n,g)
     
