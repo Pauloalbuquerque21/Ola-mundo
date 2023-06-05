4d643ad6bf36736fc7af13059153480b6e9f1f9f
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-06-05 07:11:34 -0300

    Create exercicio 104.py

---
+++ python/python mundo 3/funções 2/exercicio 104.py
@@ -0,0 +1,15 @@
+
+
+def leiaInt(a=0):
+
+    if len(a) == 0:
+        print('ERRO! Digite um número inteiro válido.')
+    else:
+        a = str(a)
+        if a.isnumeric():
+            a = int(a)
+            print(f'Você digitou o número {a}')
+        else:
+            print('ERRO! Digite um número inteiro válido.')
+        return a
+n = leiaInt(input('Digite um número:'))
