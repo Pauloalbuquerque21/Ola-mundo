f68e97b0ccc450f4b3944cff0d406423e109b144
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-06-09 20:25:15 -0300

    processo

--- python/python mundo 3/Môdulos e Pacotes/ex108/moeda.py
+++ python/python mundo 3/Môdulos e Pacotes/ex108/moeda.py
@@ -1,23 +1,27 @@
-def metade(a):
+def metade(a = 0):
     result = 0
     result = a/2
     return result
-def dobro(b):
+def dobro(b = 0):
     resultdobro = 0
     resultdobro = b * 2
     return resultdobro
-def aumentar(c):
+def aumentar(c = 0):
     resultaum = 0
     porcento = 0
     porcento = (10*c)/100
     resultaum = c + porcento
     return resultaum
-def reduzindo(d):
+def reduzindo(d = 0):
     resultreduz = 0 
     porcento = 0
     porcento = (13*d)/100
     resultreduz = d + porcento
     return resultreduz
+
+def moeda(preço = 0, moeda ='R$'):
+    return f'{moeda}{preço:.2f}'.replace('.',',')
+
 
 
     
