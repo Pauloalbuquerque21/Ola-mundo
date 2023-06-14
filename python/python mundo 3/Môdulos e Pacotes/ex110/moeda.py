d81543f70e81ba5bc0fa0f5c510a81e6fad86a41
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-06-14 06:41:27 -0300

    em processo

---
+++ python/python mundo 3/Môdulos e Pacotes/ex110/moeda.py
@@ -0,0 +1,31 @@
+def metade(met = 0,b=False):
+    result = 0
+    result = met/2
+    return result if b is False else moeda(result)
+def dobro(dob = 0,b=False ):
+    resultdobro = 0
+    resultdobro = dob * 2
+    return resultdobro if resultdobro is False else moeda(resultdobro)
+def aumentar(aum = 0,porc=0,b = False):
+    resultaum = 0
+    porcento = 0
+    porcento = (porc*aum)/100
+    resultaum = aum + porcento
+    return resultaum if resultaum is False else moeda(resultaum)
+def reduzindo(red = 0,porc = 0,b=False):
+    resultreduz = 0 
+    porcento = 0
+    porcento = (porc*red)/100
+    resultreduz = red - porcento
+    return resultreduz if resultreduz is False else moeda(resultreduz)
+
+def moeda(preço = 0, moeda ='R$'):
+    return f'{moeda}{preço:8.2f}'.replace('.',',')
+def resumo(resultado,aument,reduç):
+    print('-'*20)
+    print('RESUMO DO VALOR')
+    print('-'*20)
+    print(f'Preço analisado: {moeda(resultado)} é {metade(resultado,True)}')
+    print(f'Dobro do preço: {moeda(resultado)} é {dobro(resultado,True)}')
+    print(f'{aument}% de aumento: {aumentar(resultado,aument)}')
+    print(f'{reduç}% de redução: {reduzindo(resultado,reduç)}')
---
+++ python/python mundo 3/Môdulos e Pacotes/exercicio 110.py
@@ -0,0 +1,5 @@
+from ex110 import moeda
+p = float (input('Digite o preço:R$'))
+aum = float(input('Quanto de aumento, em porcentagem:'))
+red = float(input('Quanto de redução, em porcentagem:'))
+moeda.resumo(p,aum,red)
