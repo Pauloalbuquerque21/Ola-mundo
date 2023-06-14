59395ea5b8fb18af470b627470c22584da4ef816
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-06-14 07:18:56 -0300

    parei na atividade 111

---
+++ python/python mundo 3/Môdulos e Pacotes/exercicio 111.py
@@ -0,0 +1,5 @@
+from ex111.utildadescev.moeda import moeda
+p = float (input('Digite o preço:R$'))
+aum = float(input('Quanto de aumento, em porcentagem:'))
+red = float(input('Quanto de redução, em porcentagem:'))
+moeda.resumo(p,aum,red)
