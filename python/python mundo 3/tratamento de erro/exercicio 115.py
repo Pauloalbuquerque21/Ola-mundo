8af687595eaaac509793bc503bb34d1078a337cf
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-06-28 08:03:49 -0300

    em processo

--- python/python mundo 3/tratamento de erro/exercicio 115.py
+++ python/python mundo 3/tratamento de erro/exercicio 115.py
@@ -1,2 +1,3 @@
 from opções import opções
+opções.menu(['Opção 1','Opção 2','Opção 3'])
 inf = opções.inf('opções:')
--- python/python mundo 3/tratamento de erro/opções/opções.py
+++ python/python mundo 3/tratamento de erro/opções/opções.py
@@ -1,21 +1,10 @@
-def inf(a):
-    while True:
-        print('-'*20)
-        print('MENU PRINCIPAL')
-        print('-'*20)
-        print('1 - Ver pessoas cadastradas\n2 - Cadastrar nova pessoa\n3 - Sair do Sistema')
+def leiaint(msg):
+    While True:
         try:
-            n1 = int(input(a))
-        except:
-            print('Erro!Digite um número inteiro válido')
-        else:
-            if n1 == 1 or n1 == 2 or n1 == 3:
-                if n1 == 3:
-                    return n1
-                else:
-                    print('-'*20)
-                    print(f'Opção {n1}')
-                    print('-'*20)
-            else:
-                print('Digite um valor válido')
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('Erro: por favor, dogote um número inteiro válido.')
+            continue
+        except (KeyboardInterrupt):
+
                 
