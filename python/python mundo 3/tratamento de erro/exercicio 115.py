55abf88a85c2024d65bf08a51d249c13d1887539
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-06-29 07:43:34 -0300

    em processo

--- python/python mundo 3/tratamento de erro/exercicio 115.py
+++ python/python mundo 3/tratamento de erro/exercicio 115.py
@@ -1,3 +1,15 @@
 from opções import opções
-opções.menu(['Opção 1','Opção 2','Opção 3'])
-inf = opções.inf('opções:')
+from time import sleep
+
+while True:
+    resposta = opções.menu(['Ver pessoas cadastradas','Cadastrar nova Pessoa','Sair do sistema'])
+    if resposta == 1:
+        opções.cabeçalho('Opção 1')
+    elif resposta == 2:
+        opções.cabeçalho('Opção 2')
+    elif resposta == 3:
+        opções.cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('Erro! Digite uma opção válida')
+    sleep(2)
--- python/python mundo 3/tratamento de erro/opções/opções.py
+++ python/python mundo 3/tratamento de erro/opções/opções.py
@@ -1,8 +1,30 @@
 def leiaint(msg):
-    While True:
+    while True:
         try:
             n = int(input(msg))
         except (ValueError, TypeError):
             print('Erro: por favor, dogote um número inteiro válido.')
             continue
         except (KeyboardInterrupt):
+            print('Usuário preferiu não digitar esse número.')
+            return 0 
+        else:
+            return n
+
+def linha(tam=42):
+    return '-' * tam
+
+def cabeçalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+def menu(lista):
+    cabeçalho('MENU PRINCIPAL')
+    c = 1 
+    for item in lista:
+        print(f'{c}-{item}')
+        c += 1
+    print(linha())
+    opc = leiaint('Sua opção:')
+    return opc
