405098e4584a0bd8986397a1d755ac8951801c78
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-01-31 22:05:32 -0300

    projeto de automatização

--- semanapython/aula1.py
+++ semanapython/aula1.py
@@ -1,5 +1,15 @@
 import pyautogui
+import time
 
 pyautogui.PAUSE = 1
-for c in range(0,2):
-    pyautogui.hotkey('Ctrl','Alt','Delete')
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(5)
+pyautogui.write('https://outlook.live.com/mail/0/')
+pyautogui.press('enter')
+time.sleep(5)
+pyautogui.click(x=1753,y=255)
+time.sleep(5)
+pyautogui.click(x=1823,y=256)
+
