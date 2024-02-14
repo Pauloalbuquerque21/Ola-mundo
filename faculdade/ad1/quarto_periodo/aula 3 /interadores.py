2cb26075e751ad0f0997b0b3b44c51d39c91978d
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-02-14 10:52:56 -0300

    problema com o codigo

---
+++ faculdade/ad1/quarto_periodo/aula 3 /interadores.py
@@ -0,0 +1,23 @@
+class dados:
+    def __init__(self,limite):
+        self.limite = limite
+        self.init = 0
+
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.init < self.limite:
+            self.init += 1
+            return self.init
+        else:
+            raise StopIteration
+        
+
+teste = dados(10)
+
+for c in 10:
+    print(c)
+
+
