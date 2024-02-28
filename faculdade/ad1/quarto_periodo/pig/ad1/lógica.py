5079f9686285f1c190e9eeeafa34ccfc0780ba1c
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-02-28 09:35:50 -0300

    vou criar todas as lista e depois adicionar ao sudoko

--- faculdade/ad1/quarto_periodo/pig/ad1/lógica.py
+++ faculdade/ad1/quarto_periodo/pig/ad1/lógica.py
@@ -1,32 +1,77 @@
 import random
 
 listas=[[],[],[],[],[],[],[],[],[],[],[]]
-lista_ale = list()
+lista_1 = list()
+lista_2 = list()
 
 clock = 0
 
+while True:
+    if len(lista_1) == 0:
+        aleatorio1 = random.randint(1,9)
+        lista_1.append(aleatorio1)
+    else:
+        aleatorio1 = random.randint(1,9)
+        if aleatorio1 not in lista_1:
+            lista_1.append(aleatorio1)
+        else:
+            print('já adicionado')
 
-for linhas in listas:#linhas
-    clock +=1
-    if (clock != 4) and (linhas != 8):
-        while True:
-            if len(lista_ale):
-                aleatorio1 = random.randirt(1,9)
-            else:
-                aleatorio1 = random.randirt(1,9)
-                if aleatorio1 in lista_ale:
-                     pass
-                else:
-                     lista_ale.append(aleatorio1)
-            if len(lista_ale) == 9:
-                 break
+    if len(lista_1) == 9:
+        break
+while True:
+    if len(lista_2) == 0:
+        aleatorio1 = random.randint(1,9)
+        if aleatorio1 != lista_1[0]:
+            lista_2.append(aleatorio1)
+            print(aleatorio1)
+        else:
+            print('termino')
+    else:
+        aleatorio1 = random.randint(1,9)
+        print(aleatorio1)
+        if (aleatorio1 not in lista_1) and (aleatorio1 != lista_1[len(lista_2)]):
+            lista_1.append(aleatorio1)
+        else:
+            print('já adicionado')
+
+    if len(lista_2) == 9:
+        break
+
+
+print(f'lista1:{lista_1}')
+print(f'Lista2:{lista_2}')
+
+
+
+
+
+
+
+
+
+
+#for linhas in listas:#linhas
+#    clock +=1
+#    if (clock != 4) and (linhas != 8):
+#        while True:
+#            if len(lista_ale):
+#                aleatorio1 = random.randirt(1,9)
+#            else:
+#                aleatorio1 = random.randirt(1,9)
+#                if aleatorio1 in lista_ale:
+#                     pass
+#                else:
+#                     lista_ale.append(aleatorio1)
+#            if len(lista_ale) == 9:
+#                 break
                  
 
-        for colunas in range(1,12):#colunas
+#        for colunas in range(1,12):#colunas
 
-                    for c3 in range(aleatorio1,aleatorio2):
+#                    for c3 in range(aleatorio1,aleatorio2):
 
-    else:
-        for c2 in range(1,12):
-            c.append('-')
-        print(c)
+#    else:
+#        for c2 in range(1,12):
+#            c.append('-')
+#        print(c)
--- faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
+++ faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
@@ -2,17 +2,17 @@ import random
 lista_ale = list()
 
 while True:
-    if len(lista_ale):
+    if len(lista_ale) == 0:
         aleatorio1 = random.randint(1,9)
         lista_ale.append(aleatorio1)
         print(aleatorio1)
     else:
         aleatorio1 = random.randint(1,9)
         print(aleatorio1)
-        if aleatorio1 in lista_ale:
-            pass
-        else:
+        if aleatorio1 not in lista_ale:
             lista_ale.append(aleatorio1)
+        else:
+            print('já adicionado')
 
     if len(lista_ale) == 9:
         break
