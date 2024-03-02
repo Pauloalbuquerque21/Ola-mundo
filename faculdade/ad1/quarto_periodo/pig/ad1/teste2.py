3498976c8387aad2b3a7a89fea5869010848821e
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-03-02 12:48:28 -0300

    etapa para criar as lista ok, porém tenho que resolver repetições em colunas

--- faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
+++ faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
@@ -1,43 +1,41 @@
 import random
 
 listas=[[],[],[],[],[],[],[],[],[]]
-lista_1 = list()
-lista_2 = list()
 
 for c in range(0,9):
-    if c == 0:
-        while True: # PRIMEIRA LINHA 
-            if len(listas[0]) == 0:
-                aleatorio1 = random.randint(1,9)
-                listas[0].append(aleatorio1)
-            else:
-                aleatorio1 = random.randint(1,9)
-                if aleatorio1 not in listas[0]:
+        if c == 0:#PRIMEIRA LINHA DO SUDUKO
+            while True: # PRIMEIRA LINHA 
+                if len(listas[0]) == 0:
+                    aleatorio1 = random.randint(1,9)
                     listas[0].append(aleatorio1)
                 else:
-                    print('já adicionado')
+                    aleatorio1 = random.randint(1,9)
+                    if aleatorio1 not in listas[0]:
+                        listas[0].append(aleatorio1)
+                    else:
+                        print(f'já adicionado{c}')
 
-            if len(listas[0]) == 9:
-                break
-    else:
-        while True: # SEGUNDA LINHA 
-            if len(listas[c]) == 0:
-                aleatorio1 = random.randint(1,9)
-                if aleatorio1 != listas[c-1][0]:
-                    listas[c].append(aleatorio1)
+                if len(listas[0]) == 9:
+                    break
+        elif 0 < c < 9:#as DEMAIS LINHAS DO SUDOKU
+            while True: # SEGUNDA LINHA 
+                if len(listas[c]) == 0:
+                    aleatorio1 = random.randint(1,9)
+                    if aleatorio1 != listas[c-1][0]:
+                        listas[c].append(aleatorio1)
+                        print(aleatorio1)
+                    else:
+                        print('termino')
+                else:
+                    aleatorio1 = random.randint(1,9)
                     print(aleatorio1)
-                else:
-                    print('termino')
-            else:
-                aleatorio1 = random.randint(1,9)
-                print(aleatorio1)
-                if (aleatorio1 not in listas[c]) and (aleatorio1 != listas[c-1][len(listas[c])-1]):
-                    listas[c].append(aleatorio1)
-                else:
-                    print('já adicionado')
+                    if (aleatorio1 not in listas[c]) and (aleatorio1 != listas[c-1][len(listas[c])-1]):
+                        listas[c].append(aleatorio1)
+                    else:
+                        print('já adicionado')
 
-            if len(listas[c]) == 9:
-                break
+                if len(listas[c]) == 9:
+                    break
 
 
 for c1 in listas:
