60ed891b30e12cbe0659252108574f1a0163ef92
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-03-04 13:12:15 -0300

    entendi o problemas, não é de logica, mas sim sobre não poder concluir um lista

--- faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
+++ faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
@@ -1,6 +1,7 @@
 import random
 
 listas=[[],[],[],[],[],[],[],[],[]]
+
 quantidade = 0
 
 for c in range(0,9):
@@ -9,7 +10,7 @@ for c in range(0,9):
                 if len(listas[0]) == 0:#Parte de ingluir o primeiro elemento 
                     aleatorio1 = random.randint(1,9)
                     listas[0].append(aleatorio1)
-                else:#parte de incluir os demais elementos para a linha
+                else:#Parte de incluir os demais elementos para a linha
                     aleatorio1 = random.randint(1,9)
                     if aleatorio1 not in listas[0]:
                         listas[0].append(aleatorio1)
@@ -18,12 +19,11 @@ for c in range(0,9):
 
                 if len(listas[0]) == 9:
                     break
-        elif 0 < c < 9:#As DEMAIS LINHAS DO SUDOKU
+        else:#As DEMAIS LINHAS DO SUDOKU.
             quantidade += 1
-            print(listas) 
-            while True: # SEGUNDA LINHA
-                vezes = 0
-                vezes2 = 0 
+            while True: #SEGUNDA LINHA.
+                vezes = 0 #Quantidade de vezes que irá conferir na lista c, elemento 0.
+                vezes2 = 0 #Quantidade de vezes que irá conferir na lista c, outros elementos.
                 if len(listas[c]) == 0:
                     aleatorio1 = random.randint(1,9)
                     #verificar se a listas anteriores no elemento posição 0 existe elemento igual: 
@@ -32,21 +32,25 @@ for c in range(0,9):
                             vezes += 1
                         else:
                             print(f'Já existe {aleatorio1}')
-                    if  vezes == (quantidade):
+                    if  vezes == quantidade:
                         listas[c].append(aleatorio1)
-                        print(aleatorio1)
-                        print(f'adicionado { aleatorio1} listas{c}')
                     else:
                         print(f'já adicionado {c}')
 
-                #adicionar os demais elementos:
+                #Adicionar os demais elementos:
                 else:
+                    print(listas)
                     aleatorio1 = random.randint(1,9)
-                    print(aleatorio1)
-                    for quant in range(quantidade-1,-1,-1):
-                        if (aleatorio1 not in listas[c]) and (aleatorio1 != listas[quant][len(listas[c])]):
-                            vezes2+=1
+                    print(f'{aleatorio1}')
+                    for quant2 in range(quantidade-1,-1,-1):
+                        if (aleatorio1 not in listas[c]) and (aleatorio1 != listas[quant2][len(listas[c])]):
+                            vezes2 += 1
+                            print(f'vezes:{vezes2}')
+                            print(f'quantiadde:{quantidade}')
+                            print(f'lista:{c}')
+                            print(f'Valor aleatorio {aleatorio1}')
                     if vezes2 == quantidade:
+                        print(f'adicionou {aleatorio1}')
                         listas[c].append(aleatorio1)
                     else:
                         print('já adicionado')
