9e12e03c9e485fc5cbc7b703b093a2a8119f8a80
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-03-06 10:08:15 -0300

    agora consegui resolver a lofica, temos que focar na estética

---
+++ faculdade/ad1/quarto_periodo/pig/ad1/modelagem.py
@@ -0,0 +1,9 @@
+
+listas =[[1,2,3,4,5],[2,3,4,5,6,7]]
+
+coluna = 1
+valor = 6
+linha = 0
+ 
+listas[linha][coluna] = 6
+print(listas)
--- faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
+++ faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
@@ -2,62 +2,27 @@ import random
 
 listas=[[],[],[],[],[],[],[],[],[]]
 
+for c in range(0,9):
+    for c2 in range(0,9):
+        listas[c].append(2)
 quantidade = 0
 
-for c in range(0,9):
-        if c == 0:#PRIMEIRA LINHA DO SUDUKO
-            while True: # PRIMEIRA LINHA 
-                if len(listas[0]) == 0:#Parte de ingluir o primeiro elemento 
-                    aleatorio1 = random.randint(1,9)
-                    listas[0].append(aleatorio1)
-                else:#Parte de incluir os demais elementos para a linha
-                    aleatorio1 = random.randint(1,9)
-                    if aleatorio1 not in listas[0]:
-                        listas[0].append(aleatorio1)
-                    else:
-                        print(f'já adicionado{c}')
+linha = int(input('Digite a linha:'))
+coluna = int(input('Digite a coluna:'))
+valor = int(input('digite o valor '))
 
-                if len(listas[0]) == 9:
-                    break
-        else:#As DEMAIS LINHAS DO SUDOKU.
-            quantidade += 1
-            while True: #SEGUNDA LINHA.
-                vezes = 0 #Quantidade de vezes que irá conferir na lista c, elemento 0.
-                vezes2 = 0 #Quantidade de vezes que irá conferir na lista c, outros elementos.
-                if len(listas[c]) == 0:
-                    aleatorio1 = random.randint(1,9)
-                    #verificar se a listas anteriores no elemento posição 0 existe elemento igual: 
-                    for quant in range(quantidade-1,-1,-1):
-                        if aleatorio1 != listas[quant][0]:
-                            vezes += 1
-                        else:
-                            print(f'Já existe {aleatorio1}')
-                    if  vezes == quantidade:
-                        listas[c].append(aleatorio1)
-                    else:
-                        print(f'já adicionado {c}')
 
-                #Adicionar os demais elementos:
-                else:
-                    print(listas)
-                    aleatorio1 = random.randint(1,9)
-                    print(f'{aleatorio1}')
-                    for quant2 in range(quantidade-1,-1,-1):
-                        if (aleatorio1 not in listas[c]) and (aleatorio1 != listas[quant2][len(listas[c])]):
-                            vezes2 += 1
-                            print(f'vezes:{vezes2}')
-                            print(f'quantiadde:{quantidade}')
-                            print(f'lista:{c}')
-                            print(f'Valor aleatorio {aleatorio1}')
-                    if vezes2 == quantidade:
-                        print(f'adicionou {aleatorio1}')
-                        listas[c].append(aleatorio1)
-                    else:
-                        print('já adicionado')
+confere = 0
+aleatorio1 = valor
+for quant2 in range(linha-1,-1,-1):
+    if (aleatorio1 not in listas[linha]) and (aleatorio1 != listas[quant2][coluna]):
+        confere += 1
+if confere == linha:
+    print(f'adicionou {aleatorio1}')
+    listas[linha][coluna] = aleatorio1
+else:
+    print('já adicionado')
 
-                if len(listas[c]) == 9:
-                    break
 
-print(f'Quantidade:{quantidade}')
 for c1 in listas:
     print(c1)
