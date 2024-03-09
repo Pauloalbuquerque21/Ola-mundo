81573616539dd3a44901d860f4ef0de687954024
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-03-09 13:55:36 -0300

    fazer a regra dentro do quadrado

--- faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
+++ faculdade/ad1/quarto_periodo/pig/ad1/teste2.py
@@ -7,7 +7,7 @@ for c in range(0,9):
 erros = 0
 jogadas = 0
 
-while True:
+while True:#loop do jogo
     lista_visual = list()
 
     for c in range(0,9):
@@ -18,16 +18,26 @@ while True:
     coluna = int(input('Digite a coluna:'))
     valor = str(input('digite o valor '))
 
-
-    confere = 0
-    aleatorio1 = valor
+    #veriaveis:
+    confere_inferior = 0
+    confere_superior = 0
+    confere_linha = 0
+    # conferir se está na linha:
+    if valor not in listas[linha]:
+        confere_linha += 1
+    #verificar as linhas inferiores, mesma coluna:
     for quant2 in range(linha-1,-1,-1):
-        if (aleatorio1 not in listas[linha]) and (aleatorio1 != listas[quant2][coluna]):
-            confere += 1
-    if confere == linha:
+        if valor != listas[quant2][coluna]:
+            confere_inferior += 1
+    #verificar linhas superiores, da mesma coluna:
+    for superior in range(linha+1,9):
+        if valor != listas[superior][coluna]:
+            confere_superior += 1
+    
+    if (confere_inferior == linha) and (confere_superior == (9-(linha+1))) and (confere_linha == 1):
         jogadas += 1
-        print(f'adicionou {aleatorio1}')
-        listas[linha][coluna] = aleatorio1
+        print(f'adicionou {valor}')
+        listas[linha][coluna] = valor
         if jogadas == 5:
             break
     else:
