da4e28b7608e283899e481c6eb6b05fbbf46cf55
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-03-20 10:17:50 -0300

    teste

---
+++ faculdade/ad1/quarto_periodo/pig/estudando_p1.py
@@ -0,0 +1,3 @@
+x = int(input('Digite:'))
+assert x ==5, "valor diferênte de 5 "
+print('Apost o assert')
---
+++ faculdade/ad1/quarto_periodo/pig/verificando.py
@@ -0,0 +1,4 @@
+lista =[1,2,3,4,5]
+
+for indice, info in enumerate(lista):
+    print(f'Indice:{indice} e info:{info}')
