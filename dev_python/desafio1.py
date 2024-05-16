ee450f0396ab066cf1dbe65e5ec627b1033a29f7
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-05-16 08:16:40 -0300

    class conta criada

--- dev_python/desafio1.py
+++ dev_python/desafio1.py
@@ -5,5 +5,35 @@ class conta():
         self._agencia = agencia
         self._cliente = cliente
         self._historico = historico
+    def saldo2(self):
+        return self._saldo
     
-    
+    def novo_cliente(self):
+        print(f'CLiente{self._cliente}\nNúmero da conta:{self._numero}')
+
+    def sacar(self,valor):
+        if valor < self._saldo:
+            self._saldo -= valor
+            return True
+        else:
+            return False
+        
+    def depositar(self,valor):
+        if valor > 0 :
+            self._saldo += valor
+            return True
+        else:
+            return False
+
+
+cliente = conta(200,1,2,'Carlos',1)
+
+#print(cliente._saldo)
+
+print(cliente.saldo2())
+cliente.novo_cliente()
+print(cliente.sacar(100))
+print(cliente.saldo2())
+cliente.depositar(500)
+print(cliente.saldo2())
+
