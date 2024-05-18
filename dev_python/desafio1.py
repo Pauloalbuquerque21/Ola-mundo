8bfa863d9bc9951e73c6fa3eef0ba8ada1dbee84
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-05-18 09:30:58 -0300

    problemas com a class histórico

--- dev_python/desafio1.py
+++ dev_python/desafio1.py
@@ -1,10 +1,15 @@
-class conta():
+class conta:
+    #listas que armazenão as transações, com o objetivo de ter um histórico.
+
+
     def __init__(self,saldo,numero,agencia,cliente,historico):
         self._saldo = saldo
         self._numero = numero
         self._agencia = agencia
         self._cliente = cliente
         self._historico = historico
+        self.transacoes_sacar = list()
+        self.transacoes_depositar = list()
 
     def saldo2(self):
         return self._saldo
@@ -15,6 +20,8 @@ class conta():
     def sacar(self,valor):
         if valor < self._saldo:
             self._saldo -= valor
+            self.transacoes_sacar.append(valor)
+            print(self.transacoes_sacar)
             return True
         else:
             return False
@@ -22,10 +29,13 @@ class conta():
     def depositar(self,valor):
         if valor > 0 :
             self._saldo += valor
+            self.transacoes_depositar.append(valor)
+            print(self.transacoes_depositar)
             return True
         else:
             return False
-#obs: quando você usa o **kw você tem que definir os atributos      
+#obs: quando você usa o **kw você tem que definir os atributos
+#class filho de conta      
 class conta_corrente(conta):
     def __init__(self,limites,limites_saques,**kw):
         self.limites = limites
@@ -39,6 +49,17 @@ class conta_corrente(conta):
         else:
             return False
         
+class historico(conta):
+    def __init__(self,**kw):
+        super().__init__(**kw)
+
+    def historico_transacao():
+        transactions = len(self.transacoes_sacar) + len(self.transacoes_depositar)
+        print(transactions)
+
+
+        
+
 
 
 cliente = conta(200,1,2,'Carlos',1)
@@ -56,4 +77,6 @@ print('Cliente 2:')
 cliente2 = conta_corrente(limites=1000,limites_saques=100,saldo=200,numero=1,agencia=3,cliente='Paulo',historico=1)
 cliente2.novo_cliente()
 print(cliente2.sacar(101))
+
+cliente
 
